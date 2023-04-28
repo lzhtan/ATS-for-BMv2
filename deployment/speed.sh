14bf1e39a185e2795dc013ea1b3fe05ae8818eb0
Tan <lzhtan@bjtu.edu.cn>
2023-04-28 13:48:40 +0800

    add

---
+++ deployment/speed.sh
@@ -0,0 +1,6 @@
+ethtool -K enp15s0 gso off gro off tso off
+ethtool -K enp14s0 gso off gro off tso off
+ethtool -K enp13s0 gso off gro off tso off
+ethtool -K enp12s0 gso off gro off tso off
+ethtool -K enp11s0 gso off gro off tso off
+ethtool -K enp10s0 gso off gro off tso off
